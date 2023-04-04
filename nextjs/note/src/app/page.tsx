e1b16ed7a355d27d8e761c8501640fbaff420b75
jje0ngyu <naniaerang@naver.com>
2023-04-04 20:46:08 +0900

    04-04

--- nextjs/note/src/app/page.tsx
+++ nextjs/note/src/app/page.tsx
@@ -1,3 +1,24 @@
+
+
+
+ㅁㄴㅁㄴㄴㅇㅁㄴ ctrl+d abG (){
+
+}
+
+ㅁㄴㅁㄴㄴㅇㅁㄴ ctrl+d GGdgsG (){
+  <div>
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>
+              
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 export default function Home() {
   return <h1>홈페이지다!!</h1>;
 }
